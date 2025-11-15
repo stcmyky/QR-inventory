1be4862388582fd310ccb372d95cb25269d66746
stcmyky <83471697+stcmyky@users.noreply.github.com>
2025-11-15 20:54:24 +0100

    Delete lib directory

--- lib/utils/storage_persistent.dart
+++
@@ -1,5 +0,0 @@
-import 'storage_persistent_stub.dart'
-    if (dart.library.html) 'storage_persistent_web.dart';
-
-/// Returns true when persistent storage was granted (web only), false otherwise.
-Future<bool> requestPersistentStorage() => requestPersistentStorageImpl();
--- lib/utils/storage_persistent_stub.dart
+++
@@ -1,5 +0,0 @@
-// Non-web stub.
-Future<bool> requestPersistentStorageImpl() async {
-  // Not available on non-web platforms.
-  return false;
-}
